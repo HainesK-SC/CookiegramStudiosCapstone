4aceb7afbf23eaf6c9e85aca874038234c0ab7e0
Kyle Haines <hainesk@sheridancollege.ca>
2026-02-18 12:23:21 -0500

    initial commit

---
+++ src/main/java/com/cookiegramstudios/cookiegram/CookieGramStudiosProjectApplication.java
@@ -0,0 +1,13 @@
+package com.cookiegramstudios.cookiegram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookieGramStudiosProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CookieGramStudiosProjectApplication.class, args);
+	}
+
+}
