1042681a80685c8e282a27ca7e25dff232851f19
Kyle Haines <hainesk@sheridancollege.ca>
2026-02-22 12:38:38 -0500

    Added OrderPromotion repository layer

---
+++ src/main/java/com/cookiegramstudios/cookiegram/promotion/OrderPromotionRepository.java
@@ -0,0 +1,40 @@
+package com.cookiegramstudios.cookiegram.promotion;
+
+import java.time.LocalDateTime;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Repository interface for {@link OrderPromotion} relational entity.
+ * <p>
+ *     Provides CRUD operations and custom queries.
+ *     Extends {@link JpaRepository}
+ * </p>
+ *
+ * @author Kyle Haines
+ * @date 2026-02-22
+ * @version 1.0
+ */
+
+public interface OrderPromotionRepository extends JpaRepository<OrderPromotion, Long> {
+	/**
+     * Find an OrderPromotion(s) by promotion code
+     * @param the unique promotion code
+     * @return List: OrderPromotion - A list of OrderPromotion object(s) of the given promotion code
+     */
+	OrderPromotion findByPromoCode(String promoCode);
+	
+	/**
+     * Find a OrderPromotion(s) by the amount discounted
+     * @param the dollar value of discount applied to the order
+     * @return List: OrderPromotion - A list of OrderPromotion object(s) of the given discount amount
+     */
+	OrderPromotion findByDiscountAmt(double discountAmt);
+	
+	/**
+     * Find OrderPromotion(s) by the date and time the promotion was applied
+     * @param the date and time the order was placed and promotion was applied
+     * @return List: OrderPromotion - List of OrderPromotion object(s) that were applied at the same time
+     */
+	OrderPromotion findByAppliedAt(LocalDateTime appliedAt);
+}
