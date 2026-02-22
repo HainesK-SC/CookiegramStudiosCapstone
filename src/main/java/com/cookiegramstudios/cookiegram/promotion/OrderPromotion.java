d871224ea24fdb762a2fb9660b814ef93734cbc3
Kyle Haines <hainesk@sheridancollege.ca>
2026-02-22 12:05:26 -0500

    Added class comment and attributes

--- src/main/java/com/cookiegramstudios/cookiegram/promotion/OrderPromotion.java
+++ src/main/java/com/cookiegramstudios/cookiegram/promotion/OrderPromotion.java
@@ -1,5 +1,30 @@
 package com.cookiegramstudios.cookiegram.promotion;
 
-public class OrderPromotion {
+import java.time.LocalDateTime;
 
+/**
+ * Relational entity that represents the relationship between Order and Promotion entities.
+ * <p>
+ * An order may have 0 or more promotions on it, and a promotion may be on 0 or more Orders.
+ * This relational entity handles that relationship, by allowing multiple promotions to be a part
+ * of the same order.
+ * </p>
+ * <p>
+ * The entity includes the following key attributes:
+ * </p>
+ * <ul>
+ * <li><b>promotionId: String</b> - Unique identifier for the promotion</li>
+ * <li><b>discountAmt: Double</b> - The total dollar amount of the discount</li>
+ * <li><b>appliedAt: LocalDateTime</b> - The date and time at which the promotion was applied (only after checkout)</li>
+ * </ul>
+ *
+ * @author Kyle Haines
+ * @date 2026-02-22
+ * @version 1.0
+ */
+
+public class OrderPromotion {
+	String promotionId;
+	double discountAmt;
+	LocalDateTime appliedAt;	
 }
