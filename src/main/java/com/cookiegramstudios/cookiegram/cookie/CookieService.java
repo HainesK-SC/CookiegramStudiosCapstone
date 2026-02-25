478116e504d7f571e22170d8982a1781c9edc139
KhoaTran428 <tran187@sheridancollege.ca>
2026-02-25 13:18:46 -0500

    Make some changes for CookieService

--- src/main/java/com/cookiegramstudios/cookiegram/cookie/CookieService.java
+++ src/main/java/com/cookiegramstudios/cookiegram/cookie/CookieService.java
@@ -1,8 +1,12 @@
 package com.cookiegramstudios.cookiegram.cookie;
 import jakarta.persistence.*;
+import jakarta.transaction.Transactional;
 import lombok.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import java.util.List;
+import java.util.Optional;
 
 
 @Service
