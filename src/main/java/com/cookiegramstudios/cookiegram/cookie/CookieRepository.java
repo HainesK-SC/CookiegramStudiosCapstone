dc84c6f9a7df12ffad83beae7b80fc3d9187b13d
KhoaTran428 <tran187@sheridancollege.ca>
2026-02-25 13:25:49 -0500

    Fixing the Repository

--- src/main/java/com/cookiegramstudios/cookiegram/cookie/CookieRepository.java
+++ src/main/java/com/cookiegramstudios/cookiegram/cookie/CookieRepository.java
@@ -3,7 +3,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.util.List;
 
-public interface CookieRepository {
+public interface CookieRepository extends JpaRepository<Cookie, Long>{
     // Finds all cookies by their active status
     List<Cookie> findByActive(boolean active);
 
