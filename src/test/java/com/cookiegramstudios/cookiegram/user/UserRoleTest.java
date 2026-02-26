34b40a73a5e9568e3039144695ee26b9ce21a18a
mjsamaha <matthewsamaha@gmail.com>
2026-02-26 14:24:07 -0500

    Refactored UserRoleTest to reflect changes

--- src/test/java/com/cookiegramstudios/cookiegram/user/UserRoleTest.java
+++ src/test/java/com/cookiegramstudios/cookiegram/user/UserRoleTest.java
@@ -8,11 +8,11 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class UserRoleTest {
 
     // Tests if all roles exists
+    // Changed to reflect current roles for Sprint 1
     @Test
     void testAllRolesExists(){
         assertEquals(3, UserRole.values().length);
         assertNotNull(UserRole.ADMIN);
-        assertNotNull(UserRole.BAKER);
-        assertNotNull(UserRole.COURIER);
+        assertNotNull(UserRole.EMPLOYEE);
     }
 }
