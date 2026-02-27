cffd26d56396ef593f7d47ef55f11d0d9ababa5b
Kyle Haines <hainesk@sheridancollege.ca>
2026-02-27 09:20:23 -0500

    Added dummy customer data for development

---
+++ src/main/resources/database/data/customer_data.sql
@@ -0,0 +1,29 @@
+-- SCRIPT TO ADD CUSTOMERS TO customer TABLE FOR DEVELOPMENT
+
+INSERT INTO customer (email, created_at, last_order_date)
+	VALUES(
+	'testemail1@test.ca',
+	'2023-03-29 10:15:30',
+	'2025-04-26 11:43:23'
+	);
+
+INSERT INTO customer (email, created_at, last_order_date)
+	VALUES(
+	'testemail2@test.ca',
+	'2024-06-01 15:15:30',
+	'2025-01-19 09:23:12'
+	);
+	
+INSERT INTO customer (email, created_at, last_order_date)
+	VALUES(
+	'testemail3@test.ca',
+	'2026-01-01 08:06:10',
+	'2026-01-01 09:01:23'
+	);
+
+INSERT INTO customer (email, created_at, last_order_date)
+	VALUES(
+	'testemail4@test.ca',
+	'2024-11-21 17:42:35',
+	'2026-02-02 11:43:23'
+	);
