983484cc1e164f23ee056f3d97844c89bf43f15e
mjsamaha <matthewsamaha@gmail.com>
2026-03-03 10:46:51 -0500

    extra

--- src/main/java/com/cookiegramstudios/cookiegram/common/exceptions/InvalidCookieDataException.java
+++ src/main/java/com/cookiegramstudios/cookiegram/common/exceptions/InvalidCookieDataException.java
@@ -1,5 +1,12 @@
 package com.cookiegramstudios.cookiegram.common.exceptions;
 
+/**
+ * Custom runtime exception thrown when Cookie data validation fails.
+ *
+ * @name Nguyen Anh Khoa Tran
+ * @date 2026-02-28
+ * @version 1.0
+ */
 public class InvalidCookieDataException extends RuntimeException {
 
     public InvalidCookieDataException(String message) {
