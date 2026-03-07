2e15ed85ff272f1277a620ec84b8f0c5ecd77272
KhoaTran428 <tran187@sheridancollege.ca>
2026-03-07 14:51:48 -0500

    Jpa Test starts a specialized, lightweight database for testing Jpa components

---
+++ src/test/java/com/cookiegramstudios/cookiegram/cookie/CookieRepositoryTest.java
@@ -0,0 +1,9 @@
+package com.cookiegramstudios.cookiegram.cookie;
+
+import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
+
+@DataJpaTest // Starts a specialized, lightweight database for testing JPA components
+public class CookieRepositoryTest {
+
+
+}
