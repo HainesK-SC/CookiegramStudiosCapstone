4858b66dda2f5cfd9034af9d5088736b16f2b967
KhoaTran428 <tran187@sheridancollege.ca>
2026-03-07 15:51:12 -0500

    Having imports imported

--- src/test/java/com/cookiegramstudios/cookiegram/receipt/OrderReceiptRepositoryTest.java
+++ src/test/java/com/cookiegramstudios/cookiegram/receipt/OrderReceiptRepositoryTest.java
@@ -1,4 +1,12 @@
 package com.cookiegramstudios.cookiegram.receipt;
+import com.cookiegramstudios.cookiegram.order.Order;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
+import org.springframework.boot.jpa.test.autoconfigure.TestEntityManager;
+
+import java.util.Optional;
+import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
 public class OrderReceiptRepositoryTest {
