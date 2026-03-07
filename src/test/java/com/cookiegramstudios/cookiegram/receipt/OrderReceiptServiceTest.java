bd2ba86b1e714837bf5555d22de7e9016454059a
KhoaTran428 <tran187@sheridancollege.ca>
2026-03-07 15:19:46 -0500

    Adding imports for Mockito and AssertEqual

--- src/test/java/com/cookiegramstudios/cookiegram/receipt/OrderReceiptServiceTest.java
+++ src/test/java/com/cookiegramstudios/cookiegram/receipt/OrderReceiptServiceTest.java
@@ -1,5 +1,19 @@
 package com.cookiegramstudios.cookiegram.receipt;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDate;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+@ExtendWith(MockitoExtension.class)
 public class OrderReceiptServiceTest {
     @Mock
     private OrderReceiptRepository receiptRepository;
