d1c0a266754c1c42c6a9786758b5341a95f74aff
KhoaTran428 <tran187@sheridancollege.ca>
2026-03-07 14:39:04 -0500

    injecting mocked repository into the service

--- src/test/java/com/cookiegramstudios/cookiegram/cookie/CookieServiceTest.java
+++ src/test/java/com/cookiegramstudios/cookiegram/cookie/CookieServiceTest.java
@@ -1,9 +1,13 @@
 package com.cookiegramstudios.cookiegram.cookie;
 
 
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 
 public class CookieServiceTest {
     @Mock // Creates a "fake" version of the respository so we don't need a real database
     private CookieRepository cookieRepository;
+
+    @InjectMocks // Automatically injects the mocked repository into our service
+    private CookieService cookieService;
 }
