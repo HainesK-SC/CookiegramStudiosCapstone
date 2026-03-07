27ccecf4bc0bbd31b040ccb01ec396fbd1af2878
KhoaTran428 <tran187@sheridancollege.ca>
2026-03-07 14:52:54 -0500

    testSaveAndFindByActice helps create one active and 1 inactive cookie

--- src/test/java/com/cookiegramstudios/cookiegram/cookie/CookieRepositoryTest.java
+++ src/test/java/com/cookiegramstudios/cookiegram/cookie/CookieRepositoryTest.java
@@ -4,6 +4,13 @@ import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
 
 @DataJpaTest // Starts a specialized, lightweight database for testing JPA components
 public class CookieRepositoryTest {
+    @Autowired
+    private CookieRepository cookieRepository;
 
+    @Test
+    void testSaveAndFindByActive() {
+        // ARRANGE: Create one active and one inactive cookie
+        Cookie activeCookie = new Cookie(null, "Sugar Cookie", "Classic", "url", 2.00, true);
+        Cookie inactiveCookie = new Cookie(null, "Old Recipe", "Discontinued", "url", 1.50, false);
 
 }
