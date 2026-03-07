17de495c099a4c54cff9d83199033bef7753c08f
KhoaTran428 <tran187@sheridancollege.ca>
2026-03-07 15:48:38 -0500

    Arrange: Create and persist a parent Order first

--- src/test/java/com/cookiegramstudios/cookiegram/receipt/OrderReceiptRepositoryTest.java
+++ src/test/java/com/cookiegramstudios/cookiegram/receipt/OrderReceiptRepositoryTest.java
@@ -8,4 +8,12 @@ public class OrderReceiptRepositoryTest {
     @Autowired
     private TestEntityManager entityManager; // Used to set up related entities like Order
 
+    @Test
+    void testFindByOrderNumber() {
+        // ARRANGE: Create and persist a parent Order first (due to OneToOne requirement)
+        Order order = new Order();
+        order.setCustomerName("Test User");
+        order.setTotalAmount(10.00);
+        entityManager.persist(order); // Saves the order so it has an ID
+
 }
