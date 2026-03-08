58279fbbf611dd04bba683bf87bf22abb1b246cb
KhoaTran428 <tran187@sheridancollege.ca>
2026-03-08 19:01:26 -0400

    Adding imports

--- src/test/java/com/cookiegramstudios/cookiegram/receipt/OrderReceiptTest.java
+++ src/test/java/com/cookiegramstudios/cookiegram/receipt/OrderReceiptTest.java
@@ -1,16 +1,8 @@
 package com.cookiegramstudios.cookiegram.receipt;
 
-import com.cookiegramstudios.cookiegram.order.Order;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
-
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.*;
-@ExtendWith(MockitoExtension.class)
+
 public class OrderReceiptTest {
     /*
      * Test that the OrderReceipt constructor correctly assigns values.
