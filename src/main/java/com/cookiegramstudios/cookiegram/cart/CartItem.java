8a470b93e9dff19aecfae57fbe9b025ffc99ea19
Kyle Haines <hainesk@sheridancollege.ca>
2026-03-15 16:05:18 -0400

    Removed unused import

--- src/main/java/com/cookiegramstudios/cookiegram/cart/CartItem.java
+++ src/main/java/com/cookiegramstudios/cookiegram/cart/CartItem.java
@@ -2,8 +2,6 @@ package com.cookiegramstudios.cookiegram.cart;
 
 import com.cookiegramstudios.cookiegram.product.Product;
 
-import lombok.Data;
-
 public class CartItem {
 	private Product productType; // currently can only be a Cookie object
 	private int itemQty;
