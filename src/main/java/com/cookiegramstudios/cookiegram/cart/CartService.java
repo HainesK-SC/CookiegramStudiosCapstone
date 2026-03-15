79f7430755af730bfeb4fe0c96a8e38dc74b0879
Kyle Haines <hainesk@sheridancollege.ca>
2026-03-15 14:56:09 -0400

    Added logic to handle adding items to a Cart session object

--- src/main/java/com/cookiegramstudios/cookiegram/cart/CartService.java
+++ src/main/java/com/cookiegramstudios/cookiegram/cart/CartService.java
@@ -1,5 +1,10 @@
 package com.cookiegramstudios.cookiegram.cart;
 
+import java.util.Optional;
+
+import com.cookiegramstudios.cookiegram.product.Product;
+import com.cookiegramstudios.cookiegram.product.ProductRepository;
+
 /**
  * Service class that will interact with the CartController to
  * perform various logical tasks related to a Cart object.
@@ -8,4 +13,26 @@ package com.cookiegramstudios.cookiegram.cart;
  */
 public class CartService {
 	
+	private final ProductRepository productRepository;
+	
+	public CartService(ProductRepository prodRepo) {
+		this.productRepository = prodRepo;
+	}
+	
+	public void addToCart(Cart cart, Long productId, int quantity) {
+		Optional<CartItem> existingItem = cart.getCartItems().stream()
+				.filter(item -> item.getProductType().getId().equals(productId))
+				.findFirst();
+		
+		if (existingItem.isPresent() ) {
+			existingItem.get().setItemQty(existingItem.get().getItemQty() + quantity);
+		}
+		else {
+			Product product = productRepository.findById(productId)
+					.orElseThrow(() -> new RuntimeException("Product not found"));
+			
+			CartItem newItem = new CartItem(product, quantity);
+			cart.getCartItems().add(newItem);
+		}
+	}
 }
