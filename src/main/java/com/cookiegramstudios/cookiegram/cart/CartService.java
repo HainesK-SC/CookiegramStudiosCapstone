eb6a3dc34252b2c87a18cc1e1788bd61d7e45651
Kyle Haines <hainesk@sheridancollege.ca>
2026-03-15 13:33:21 -0400

    Created skeleton CartService class

---
+++ src/main/java/com/cookiegramstudios/cookiegram/cart/CartService.java
@@ -0,0 +1,11 @@
+package com.cookiegramstudios.cookiegram.cart;
+
+/**
+ * Service class that will interact with the CartController to
+ * perform various logical tasks related to a Cart object.
+ * 
+ * @author Kyle Haines
+ */
+public class CartService {
+	
+}
