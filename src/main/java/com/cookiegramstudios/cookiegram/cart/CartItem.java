d7074389a9906a19e3c70ad3a005baf90ba1cb83
Kyle Haines <hainesk@sheridancollege.ca>
2026-03-15 12:34:35 -0400

    Added class to reference cart items

---
+++ src/main/java/com/cookiegramstudios/cookiegram/cart/CartItem.java
@@ -0,0 +1,8 @@
+package com.cookiegramstudios.cookiegram.cart;
+
+import com.cookiegramstudios.cookiegram.product.Product;
+
+public class CartItem {
+	private Product productType; // currently can only be a Cookie object
+	private int itemQty;
+}
