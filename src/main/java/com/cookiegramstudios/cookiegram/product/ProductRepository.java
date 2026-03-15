5d31e06e696f91171aa9455c9ffa41d4617f6f1c
Kyle Haines <hainesk@sheridancollege.ca>
2026-03-15 12:55:32 -0400

    Added various finder methods

---
+++ src/main/java/com/cookiegramstudios/cookiegram/product/ProductRepository.java
@@ -0,0 +1,19 @@
+package com.cookiegramstudios.cookiegram.product;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Simple JPA Repository layer that will interact with the Database
+ * to retrieve 
+ */
+public interface ProductRepository extends JpaRepository<Product, Long> {
+	Product findByBasePrice(double price);
+	
+	@Override
+	Optional<Product> findById(Long id);
+	
+	List<Product> findAll();
+}
