da21c4be598d867c83c03db7123a7ff3b68623a2
Kyle Haines <hainesk@sheridancollege.ca>
2026-03-15 12:28:30 -0400

    Cart package and skeleton Cart class added

---
+++ src/main/java/com/cookiegramstudios/cookiegram/cart/Cart.java
@@ -0,0 +1,13 @@
+package com.cookiegramstudios.cookiegram.cart;
+
+/**
+ * This is the intermediary step in the order process. This will allow
+ * customers to add items into their cart and choose when they would like
+ * to move forward with the order process. This provides more autonomy and
+ * ownership of the decision process from end-to-end for the customer.
+ * 
+ * @author Kyle Haines
+ */
+public class Cart {
+
+}
