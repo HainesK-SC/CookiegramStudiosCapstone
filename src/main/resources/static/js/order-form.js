35bb60f9735a9fbe5773bc91d5c6bfc1471bbbc1
Kyle Haines <hainesk@sheridancollege.ca>
2026-03-15 15:05:56 -0400

    Started item qty increment event handling

--- src/main/resources/static/js/order-form.js
+++ src/main/resources/static/js/order-form.js
@@ -1,1 +1,20 @@
-// Placeholder script for public layout pages.
+// This script handles the logic for item quantity on the /order page
+// Makes it possible for the arrows to be used to increment and decrement
+// the qty for a given item
+
+document.addEventListener('DOMContentLoaded', function() {
+	const cards = document.querySelectorAll('.order-item-card');
+	cards.forEach(function(card) {
+		const decrementBtn = card.querySelector('.qty-btn:first-of-type');
+		const incrementBtn = card.querySelector('.qty-btn:last-of-type');
+		const qtyDisplay = card.querySelector('.order-qty-number');
+		const qtyInput = card.querySelector('input[name="productQuantity"]');
+	});
+	
+	incrementBtn.addEventListener('click', function() {
+		let currentQty = parseInt(qtyDisplay.textContent);
+		currentQty++;
+		qtyDisplay.textContent = currentQty;
+		qtyInput.value = currentQty.
+	});
+});
