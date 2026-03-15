3b7b7bc8247f4087bbbcedc675248061b753222b
Kyle Haines <hainesk@sheridancollege.ca>
2026-03-15 16:04:13 -0400

    Added the addToCart method to add items to the cart - and getCartSize helper

--- src/main/java/com/cookiegramstudios/cookiegram/cart/CartService.java
+++ src/main/java/com/cookiegramstudios/cookiegram/cart/CartService.java
@@ -1,6 +1,11 @@
 package com.cookiegramstudios.cookiegram.cart;
 
+import java.util.List;
 import java.util.Optional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
 
 import com.cookiegramstudios.cookiegram.product.Product;
 import com.cookiegramstudios.cookiegram.product.ProductRepository;
@@ -11,15 +16,21 @@ import com.cookiegramstudios.cookiegram.product.ProductRepository;
  * 
  * @author Kyle Haines
  */
+
+@Service
 public class CartService {
 	
 	private final ProductRepository productRepository;
+	private final static Logger logger = LoggerFactory.getLogger(CartService.class);
 	
 	public CartService(ProductRepository prodRepo) {
 		this.productRepository = prodRepo;
 	}
 	
 	public void addToCart(Cart cart, Long productId, int quantity) {
+		logger.info("Attempting to add to cart...");
+		int initialCartSize = cart.getCartItems().size();
+		
 		Optional<CartItem> existingItem = cart.getCartItems().stream()
 				.filter(item -> item.getProductType().getId().equals(productId))
 				.findFirst();
@@ -34,5 +45,20 @@ public class CartService {
 			CartItem newItem = new CartItem(product, quantity);
 			cart.getCartItems().add(newItem);
 		}
+		
+		if (cart.getCartItems().size() > initialCartSize) {
+			logger.info("Item(s) successfully added to the cart...");
+		}
+		else {
+			logger.info("Error: Item(s) not added to the cart...");
+		}
+	}
+	
+	public int getCartSize(Cart cart) {
+		List<CartItem> cartItems = cart.getCartItems();
+		
+		int cartSize = cartItems.size();
+		
+		return cartSize;
 	}
 }
