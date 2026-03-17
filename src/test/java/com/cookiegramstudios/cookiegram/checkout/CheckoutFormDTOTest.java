7e4177c3598d94ac38607f3c634aebacbe0f1273
mjsamaha <matthewsamaha@gmail.com>
2026-03-17 12:19:26 -0400

    CheckoutFormDTO Test skeleton

--- src/test/java/com/cookiegramstudios/cookiegram/checkout/CheckoutFormDTOTest.java
+++ src/test/java/com/cookiegramstudios/cookiegram/checkout/CheckoutFormDTOTest.java
@@ -1,5 +1,77 @@
 package com.cookiegramstudios.cookiegram.checkout;
 
+
 public class CheckoutFormDTOTest {
+	
+	/**
+	 * Helpers
+	 */
+	
+	
+	
+	/**
+	 * Returns violation messages 
+	 */
+	
+	
+	/**
+	 * Valid Form
+	 */
+	
+	
+	/**
+	 * Default State
+	 */
+	
+	
+	/**
+	 * No-arg Constructor
+	 */
+	
+	
+	/**
+	 * Recipient fields
+	 */
+	// Blank recipient name
+	
+	
+	// Null recipient name
+	
+	
+	// Blank recipientStreet
+	
+	
+	// Blank recipientCity
+	
+	
+	// Blank recipientPostalCode
+	
+	
+	// Blank recipientCountry
+	
+	
+	/**
+	 * Delivery Date
+	 */
+	
+	
+	/**
+	 * Deliver Time
+	 */
+	
+	
+	/**
+	 * Sender Fiekds
+	 */
+	
+	
+	/**
+	 * All Optional Fields
+	 */
+	
+	
+	/**
+	 * Multiple Violations at once
+	 */
 
 }
