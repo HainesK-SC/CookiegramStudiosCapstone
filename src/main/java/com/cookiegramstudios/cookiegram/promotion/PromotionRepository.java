e0df0c14d4b8d19311690da080387704c5d8b714
mjsamaha <matthewsamaha@gmail.com>
2026-03-18 11:40:52 -0400

    chores(refactor) promotion package :: underlying logic is still present, just removed unecessary. comments

--- src/main/java/com/cookiegramstudios/cookiegram/promotion/PromotionRepository.java
+++ src/main/java/com/cookiegramstudios/cookiegram/promotion/PromotionRepository.java
@@ -9,8 +9,7 @@ import org.springframework.stereotype.Repository;
 /**
  * Repository interface for {@link Promotion} entity.
  * <p>
- *     Provides CRUD operations and custom queries.
- *     Extends {@link JpaRepository}
+ * Provides CRUD operations and custom queries. Extends {@link JpaRepository}
  * </p>
  *
  * @author Kyle Haines
@@ -20,45 +19,18 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PromotionRepository extends JpaRepository<Promotion, Long> {
-	/**
-     * Find a Promotion by promoCode
-     * @param the unique promotion code
-     * @return Promotion object of the given promotion code
-     */
+
 	Promotion findByPromoCode(String promoCode);
-	
-	/**
-     * Find a Promotion by promoType ($ or %, dollar, percentage)
-     * @param promoType: String - DOLLAR or PERCENT - ENUMURATION
-     * @return promotions: List - Promotion object(s) of the given promotion type
-     */
+
 	List<Promotion> findByPromoType(PromotionTypes promoType);
-	
-	/**
-     * Find a Promotion by promoValue (The dollar figure of the discount)
-     * @param promoValue: double - Dollar value of the discount offered by the promotion
-     * @return promotions: List - Promotion object(s) of the given promotion type
-     */
+
 	List<Promotion> findByPromoValue(double promoValue);
-	
-	/**
-     * Find a Promotion by the start date
-     * @param startDate: LocalDateTime - The date and time the promotion started.
-     * @return promotions: List - Promotion object(s) of the given promotion type
-     */
+
 	List<Promotion> findByStartDate(LocalDate startDate);
-	
-	/**
-     * Find a Promotion by the end date
-     * @param endDate: LocalDateTime - The date and time the promotion started.
-     * @return promotions: List - Promotion object(s) of the given promotion type
-     */
+
 	List<Promotion> findByEndDate(LocalDate endDate);
-	
-	/**
-     * Find all active promotions
-     * @param isActive: boolean - True (ACTIVE) / False (INACTIVE)
-     * @return promotions: List - Promotion object(s) of the given promotion type
-     */
-	List<Promotion> findByIsActive(boolean isActive);boolean existsByPromoCodeAndIdNot(String promoCode, long id);
+
+	List<Promotion> findByIsActive(boolean isActive);
+
+	boolean existsByPromoCodeAndIdNot(String promoCode, long id);
 }
