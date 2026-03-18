ce1cd632a976a2fa7f921bad2f0cadf7eccd3269
mjsamaha <matthewsamaha@gmail.com>
2026-03-18 12:54:23 -0400

    cartValidator utility methods to clean up the cart infrastructure

---
+++ src/main/java/com/cookiegramstudios/cookiegram/cart/CartValidator.java
@@ -0,0 +1,8 @@
+package com.cookiegramstudios.cookiegram.cart;
+
+public class CartValidator {
+	
+	private CartValidator() {
+        throw new UnsupportedOperationException("Utility class - do not instantiate");
+    }
+}
