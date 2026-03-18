85d25765bd39383289c061a8d51da162a93a7a82
mjsamaha <matthewsamaha@gmail.com>
2026-03-18 12:38:51 -0400

    chore: order creation DTO

---
+++ src/main/java/com/cookiegramstudios/cookiegram/order/OrderCreationRequest.java
@@ -0,0 +1,13 @@
+package com.cookiegramstudios.cookiegram.order;
+
+import com.cookiegramstudios.cookiegram.cart.Cart;
+
+public class OrderCreationRequest {
+	
+	private Cart cart;
+	
+	private CheckoutFormDTO checkoutForm;
+	
+	private double totalPrice;
+
+}
