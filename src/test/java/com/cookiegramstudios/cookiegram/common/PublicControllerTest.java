8e33bdf289fe3736ef7e31eac2ab0c03f5f6146f
mjsamaha <matthewsamaha@gmail.com>
2026-03-18 14:25:31 -0400

    PublicControllerTest refactoring

--- src/test/java/com/cookiegramstudios/cookiegram/common/PublicControllerTest.java
+++ src/test/java/com/cookiegramstudios/cookiegram/common/PublicControllerTest.java
@@ -17,51 +17,45 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 
-
 import com.cookiegramstudios.cookiegram.promotion.PromotionService;
 import com.cookiegramstudios.cookiegram.user.UserService;
 
 /**
  * Basic tests for {@link PublicController}.
+ * 
  * @author Matthew Samaha
  * @date 2026-02-27
  * @version 1.0
  */
 @WebMvcTest(PublicController.class)
-@AutoConfigureMockMvc(addFilters = false) // Disable security filters for testing :: fix 1 to the previous testing errors
+@AutoConfigureMockMvc(addFilters = false) // Disable security filters for testing :: fix 1 to the previous testing
+											// errors
 public class PublicControllerTest {
 
-    @Autowired
-    private MockMvc mockMvc;
+	@Autowired
+	private MockMvc mockMvc;
 
-    @MockitoBean
-    private PromotionService promotionService;
+	@MockitoBean
+	private PromotionService promotionService;
 
-    @MockitoBean
-    private UserService userService;
+	@MockitoBean
+	private UserService userService;
 
-    @Test
-    void homePageIsAccessible() throws Exception {
-        when(promotionService.getByIsActive(true)).thenReturn(Collections.emptyList());
+	@Test
+	void homePageIsAccessible() throws Exception {
+		when(promotionService.getByIsActive(true)).thenReturn(Collections.emptyList());
 
-        mockMvc.perform(get("/"))
-                .andExpect(status().isOk())
-                .andExpect(view().name("index"));
-    }
+		mockMvc.perform(get("/")).andExpect(status().isOk()).andExpect(view().name("index"));
+	}
 
-    @Test
-    void loginPageIsAccessible() throws Exception {
-        mockMvc.perform(get("/login").with(csrf()))
-                .andExpect(status().isOk())
-                .andExpect(view().name("login"));
-    }
+	@Test
+	void loginPageIsAccessible() throws Exception {
+		mockMvc.perform(get("/login").with(csrf())).andExpect(status().isOk()).andExpect(view().name("login"));
+	}
 
-    @Test
-    void loginPageWithErrorParameter() throws Exception {
-        mockMvc.perform(get("/login")
-                .param("error", "true")
-                .with(csrf()))
-                .andExpect(status().isOk())
-                .andExpect(model().attributeExists("error"));
-    }
+	@Test
+	void loginPageWithErrorParameter() throws Exception {
+		mockMvc.perform(get("/login").param("error", "true").with(csrf())).andExpect(status().isOk())
+				.andExpect(model().attributeExists("error"));
+	}
 }
