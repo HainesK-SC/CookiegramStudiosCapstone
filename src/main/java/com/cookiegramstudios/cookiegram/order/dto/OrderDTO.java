9af6ff5ab810ba5b7c37ee125402927c1a025d57
KhoaTran428 <tran187@sheridancollege.ca>
2026-03-26 21:33:15 -0400

    get and set total price

--- src/main/java/com/cookiegramstudios/cookiegram/order/dto/OrderDTO.java
+++ src/main/java/com/cookiegramstudios/cookiegram/order/dto/OrderDTO.java
@@ -42,4 +42,12 @@ public class OrderDTO {
         this.deliveryDate = deliveryDate;
     }
 
+    public double getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(double totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
 }
