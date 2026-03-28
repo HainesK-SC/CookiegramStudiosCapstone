d2459dedb4dddd480cda286be6cd61d0e44972a6
Kyle Haines <hainesk@sheridancollege.ca>
2026-03-28 11:18:45 -0400

    Added setter for createdAt member variable

--- src/main/java/com/cookiegramstudios/cookiegram/order/Order.java
+++ src/main/java/com/cookiegramstudios/cookiegram/order/Order.java
@@ -234,6 +234,10 @@ public class Order {
         this.approvedAt = approvedAt;
     }
 
+    public void setCreatedAt(LocalDateTime createdAt) {
+    	this.createdAt = createdAt;
+    }
+    
     public LocalDateTime getCreatedAt() {
         return createdAt;
     }
